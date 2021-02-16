9b518711011c516f3fa696bbe820ddb4445bde4e
JAmoMES <atipat.pa@ku.th>
2021-02-17 03:21:03 +0700

    add admin permisstion delete

--- LightManagingBot.py
+++ LightManagingBot.py
@@ -1,5 +1,4 @@
 import os
-import discord
 from discord import team, message
 from discord.client import Client
 from discord.ext import commands
@@ -18,11 +17,17 @@ db = cluster.exceed_group16
 collection = db.admin_user
 
 def info_user(message):
-    print("category : ",message.channel.name)
-    print("Author : ",message.author)
-    print("Author name : ",message.author.name)
-    print("Author ID: ",message.author.id)
-    print("message : " ,message.content)
+    try:
+        print("category : ",message.channel.name)
+        print("Author : ",message.author)
+        print("Author name : ",message.author.name)
+        print("Author ID: ",message.author.id)
+        print("message : " ,message.content)
+    except:
+        print("Author : ",message.author)
+        print("Author name : ",message.author.name)
+        print("Author ID: ",message.author.id)
+        print("message : " ,message.content)
     print('+'*30)
 
 @client.event
@@ -34,10 +39,10 @@ async def on_message(message):
     if message.author == client.user:
         return 
     info_user(message)
-    print(client.get_all_members())
     await client.process_commands(message)
     if 'นาทัน' in message.content:
         await message.channel.send('สวัสดีครับน้องๆExceed')
+        await message.channel.send('ม่อน')
     
 
 @client.command()
@@ -58,7 +63,7 @@ async def banana(message):
     await message.channel.send('https://annemurray99.files.wordpress.com/2015/03/huge-dancing-banana-2.gif')
 
 @client.command()
-async def add_admin(message):
+async def add_user(message):
     user = {}
     user['author_name'] = message.author.name + '#' + message.author.discriminator
     user['name'] = message.author.name
@@ -76,21 +81,51 @@ async def add_admin(message):
         await message.channel.send("นาทันงอง")
 
 @client.command()
-async def add_permission(message,arg):
-    filt2 = {'name':arg} 
+async def add_permission(message,*arg):
+    try:
+        a = arg[0]
+    except:
+        return await message.channel.send("นาทันงอง")
+    filt2 = {'name':arg[0]} 
     filt = {'author_name': message.author.name + '#' + message.author.discriminator }
-    admin = collection.find(filt)
-    for ele in admin:
-        admin = ele
-    if admin["permission"] == 1:
+    admin = collection.find_one(filt)
+    if admin["permission"] == 1 or admin["permission"] == 2:
+        set_per = collection.find_one(filt2)
+        print(set_per)
+        if set_per["permission"] != 0:
+            return await message.channel.send("ก้มียศละนิครับ")
         updated_content = {"$set": {'permission' : 1}}
         collection.update_one(filt2, updated_content)
         await message.channel.send("เป็น admin ละค้าบ")
     else :
         await message.channel.send("พี่ขอไม่ให้ผ่านนะ")
 
+@client.command()
+async def delete_permission(message,arg):
+    filt2 = {'name':arg} 
+    filt = {'author_name': message.author.name + '#' + message.author.discriminator }
+    admin = collection.find_one(filt)
+    if admin["permission"] == 2:
+        updated_content = {"$set": {'permission' : 0}}
+        collection.update_one(filt2, updated_content)
+        await message.channel.send("ปัดตกละค้าบ")
+    else :
+        await message.channel.send("อย่าปีนเกียว")
 
+@client.command()
+async def role(message,arg):
+    filt = {'author_name': message.author.name + '#' + message.author.discriminator }
+    admin = collection.find_one(filt)
+    if admin["permission"] == 2:
+        await message.channel.send("You are Super Admin.")
+    elif admin["permission"] == 1:
+        await message.channel.send("You are Admin.")
+    else :
+        await message.channel.send("You are User.")
 
+# @client.command()
+# async def change_color(messege,*avg):
 
+    
 if __name__ == '__main__':
     client.run(TOKEN)   
