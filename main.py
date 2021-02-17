124bb42dc0b2ef965751a0af8c60295a8537bf0b
unknown <jinnajate.a@ku.th>
2021-02-17 12:19:49 +0700

    update main

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
-improt time
+improt datetime
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_group16:7vyf5srq@158.108.182.0:2255/exceed_group16'
 mongo = PyMongo(app)
 
-admin = mongo.db.admin_user
-people = mongo.db.people
-room_info = mongo.db.room_info
+admin_db = mongo.db.admin_user
+people_db = mongo.db.people
+room_info_db = mongo.db.room_info
 
-@app.route('/create', methods=['POST'])
+@app.route('/hardware', methods=['POST'])
 def insert_one():
-    car_number = request.args.get('car_number')
     data = request.json
     myInsert = {
-            "car_number": car_number,
-            "time_in": data["time_in"],
-            "time_out": data["time_out"]
+            'Type' : {Room_info},
+            'ID' : data["ID"],
+            'r': data["r"],
+            'g': data["g"],
+            'b': data["b"],
+            'w': data["w"],
+            'Status' : 1,
+            'Time_in' : datetime.datetime(), 
+            'Time_out' : None,
+            'Discord' : - 
             }
-    myCollection.insert_one(myInsert)
-    return {'result': 'Created successfully'}
+    room_info.insert_one(myInsert)
+    return {'result': 'room_info insert successfully'}
 
 
 if __name__ == "__main__":
