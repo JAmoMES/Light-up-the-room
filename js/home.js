05b2be54986ddad3d26ea4b192f6a62e5c7d99aa
Thornthep Chomchuen <thornthep.c@ku.th>
2021-02-17 16:38:31 +0700

    add color button

--- js/home.js
+++ js/home.js
@@ -6,3 +6,11 @@
 //     if (document.readyState != "loading") callback();
 //     else document.addEventListener("DOMContentLoaded", callback);
 // }
+
+function getColor(){
+    // return array and send to database
+}
+
+function selectColor(){
+    // get color in checkbox and put in to array
+}
