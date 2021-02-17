b1c3d794486d495884cbd9da44687ffaf90b839a
unknown <jinnajate.a@ku.th>
2021-02-17 13:38:44 +0700

    update main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
-improt datetime
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_group16:7vyf5srq@158.108.182.0:2255/exceed_group16'
@@ -21,13 +21,21 @@ def insert_one():
             'b': data["b"],
             'w': data["w"],
             'Status' : 1,
-            'Time_in' : datetime.datetime(), 
+            'Time_in' : datetime.now(), 
             'Time_out' : None,
             'Discord' : - 
             }
     room_info.insert_one(myInsert)
     return {'result': 'room_info insert successfully'}
 
+@app.route('/hardware', methods=['GET'])
+def find():
+    flit = {"Status": 1}
+    query = room_info.find(flit)
+    res = []
+    for data in query:
+        res.append(data)
+    return { "result": res }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='50005', debug=True)
